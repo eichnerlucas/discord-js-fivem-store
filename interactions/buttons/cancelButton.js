9bb387d35e45479e554f473eb8603a58ab265c8f
WypalaczRun <lucaseichner@yahoo.com.br>
2023-11-28 00:47:18 -0300

    add try catch cancelbutton to show error

--- interactions/buttons/cancelButton.js
+++ interactions/buttons/cancelButton.js
@@ -3,31 +3,36 @@ const mercadopago = require("../../utils/mercadopago.js");
 const MessageEmbedUtil = require("../../utils/MessageEmbedUtil.js");
 
 async function run(interaction) {
-    const generating = MessageEmbedUtil.create("**Aguarde**", "success", "**Cancelando pagamento...**");
-    await interaction.deferUpdate();
-
-    await interaction.editReply({
-        embeds: [generating],
-        components: [],
-        files: []
-    });
-
-    const { paymentId } = client.interactionsData.get(interaction.customId + `:${interaction.message.channelId}`);
-    const response = await mercadopago.payment.cancel(paymentId);
-    const { status, external_reference } = response.response;
-
-    client.db.query('UPDATE payments SET status = ? WHERE external_ref = ?;', [status, external_reference]);
-
-    const canceled = MessageEmbedUtil.create("**Pedido cancelado com sucesso**", "success", `**Lamentamos que não tenha dado tudo certo na sua compra, esperamos vê-lo em breve...**`);
-
-    // Editar a resposta original
-    await interaction.editReply({
-        embeds: [canceled],
-        components: [],
-        files: []
-    });
-
-    client.interactionsData.delete(interaction.customId + `:${interaction.channelId}`);
+    try {
+        const generating = MessageEmbedUtil.create("**Aguarde**", "success", "**Cancelando pagamento...**");
+        await interaction.deferUpdate();
+    
+        await interaction.editReply({
+            embeds: [generating],
+            components: [],
+            files: []
+        });
+    
+        const { paymentId } = client.interactionsData.get(interaction.customId + `:${interaction.message.channelId}`);
+        const response = await mercadopago.payment.cancel(paymentId);
+        const { status, external_reference } = response.response;
+    
+        client.db.query('UPDATE payments SET status = ? WHERE external_ref = ?;', [status, external_reference]);
+    
+        const canceled = MessageEmbedUtil.create("**Pedido cancelado com sucesso**", "success", `**Lamentamos que não tenha dado tudo certo na sua compra, esperamos vê-lo em breve...**`);
+    
+        // Editar a resposta original
+        await interaction.editReply({
+            embeds: [canceled],
+            components: [],
+            files: []
+        });
+    
+        client.interactionsData.delete(interaction.customId + `:${interaction.channelId}`);
+    } catch (error) {
+        const errorEmbed = MessageEmbedUtil.create("**Erro ao Cancelar Pagamento**", "error", `**Ocorreu um erro ao cancelar seu pedido, informe a um administrador o erro:\n\`\`${error}\`\`**`);
+        interaction.editReply({ embeds: [errorEmbed], components: [], files: [] });
+    }
 }
 
 module.exports = { 
