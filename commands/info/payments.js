67db249e069101be927e85b330e02aa0ade54d9d
WypalaczRun <lucaseichner@yahoo.com.br>
2023-12-25 20:26:26 -0300

    Refactor payments command for code clarity
    Code in commands/info/payments.js has been refactored to improve readability and maintainability. Concretely, constants have been introduced for immutable values, helper functions have been created for better separation of concerns, and logic has been simplified, especially around fetching and displaying payments.

--- commands/info/payments.js
+++ commands/info/payments.js
@@ -1,67 +1,47 @@
 const { MessageEmbed } = require("discord.js");
-const moneyFormat = require('./../../utils/moneyFormat')
+const moneyFormat = require('./../../utils/moneyFormat');
+const MessageEmbedUtil = require('../../utils/MessageEmbed');
+const EMPTY_PAYMENTS_MSG = "**Empty Payments**";
+const PAYMENTS_PAGE_TITLE = "Payments Page: ";
+const SUCCESS_COLOR = '#0099ff';
+const LIMIT = 5;
+const REACTIONS = ['⬅️', '➡️'];
+const REACTION_TIME = 60000;
 
 module.exports = {
     name: "payments",
     aliases: ['pagamentos', 'payment'],
-
     run: async (client, message, args) => {
         let page = 1;
-        const limit = 5;
-
-        let offset = (page - 1) * limit;
+        let offset = (page - 1) * LIMIT;
         const discordUserId = message.author.id;
+        let collector;
 
-        const fetchPayments = () => new Promise((resolve, reject) => {
-            client.db.query('SELECT payment_id, script, status, type, price FROM payments WHERE discord_id = ? ORDER BY id DESC LIMIT ?, ?', [discordUserId, offset, limit], (err, results) => {
-                if (err) reject(err);
-                else resolve(results);
-            });
-        });
-
-        const updateEmbed = async (msg) => {
-            try {
-                let results = await fetchPayments();
-
-                const desc = results.map(result => `Payment ID: **${result.payment_id}**\nScript: ${result.script}\nValue: ${moneyFormat(result.price)}\nStatus: ${result.status}\nType: ${result.type}`).join('\n\n');
-                const embed = new MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle(`Payments Page: ${page}`)
-                    .setDescription(desc);
-
-                await msg.edit({ embeds: [embed] });
-            } catch (error) {
-                console.error('Error fetching payments:', error);
-            }
-        }
+        const fetchPayments = () => getPayments(client, discordUserId, offset);
+        const updateEmbed = async (msg) => updateMessageEmbed(msg, fetchPayments, page);
 
         try {
             const msg = await message.channel.send('Loading payments...');
+            await makeReactions(msg);
             await updateEmbed(msg);
-
-            const collector = msg.createReactionCollector({
-                filter: (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && !user.bot,
-                time: 60000,
+            collector = msg.createReactionCollector({
+                filter: (reaction, user) => REACTIONS.includes(reaction.emoji.name) && !user.bot,
+                time: REACTION_TIME,
             });
-
             collector.on('collect', async (reaction, user) => {
-                if (reaction.emoji.name === '⬅️') {
+                if (reaction.emoji.name === REACTIONS[0]) {
                     if (page > 1) {
                         page--;
-                        offset = (page - 1) * limit;
+                        offset = (page - 1) * LIMIT;
                         await updateEmbed(msg);
                     }
                 } else {
                     page++;
-                    offset = (page - 1) * limit;
+                    offset = (page - 1) * LIMIT;
                     await updateEmbed(msg);
                 }
                 await reaction.users.remove(user.id);
             });
-
-            await msg.react('⬅️');
-            await msg.react('➡️');
-
             collector.on('end', () => {
                 msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
             });
@@ -70,3 +50,41 @@ module.exports = {
         }
     },
 };
+
+function getPayments(client, discordUserId, offset) {
+    return new Promise((resolve, reject) => {
+        client.db.query('SELECT payment_id, script, status, type, price FROM payments WHERE discord_id = ? ORDER BY id DESC LIMIT ?, ?', [discordUserId, offset, LIMIT], (err, results) => {
+            if (err) reject(err);
+            else if(results.length === 0) resolve('No payments found');
+            else resolve(results);
+        });
+    });
+}
+
+function formatPaymentDescription(results) {
+    return results.map(result => `Payment ID: **${result.payment_id}**\nScript: ${result.script}\nValue: ${moneyFormat(result.price)}\nStatus: ${result.status}\nType: ${result.type}`).join('\n\n');
+}
+
+async function updateMessageEmbed(msg, fetchPayments, page) {
+    try {
+        const results = await fetchPayments();
+
+        if (typeof results === 'string') {
+            const embed = MessageEmbedUtil.create(EMPTY_PAYMENTS_MSG, "error", "No payments were found for your DiscordId")
+            await msg.edit({embeds: [embed] });
+            await msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+        } else {
+            const desc = formatPaymentDescription(results);
+            const embed = MessageEmbedUtil.create(PAYMENTS_PAGE_TITLE + page, "success", desc)
+            await msg.edit({ embeds: [embed] });
+        }
+    } catch (error) {
+        console.error('Error fetching payments:', error);
+    }
+}
+
+async function makeReactions(msg) {
+    for (let i = 0; i < REACTIONS.length; i++) {
+        await msg.react(REACTIONS[i]);
+    }
+}
